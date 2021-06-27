ee79457bf071593241ec9e8bc96ef9b54fa1d39d
argot <pablotesari@gmail.com>
2021-06-27 05:28:07 -0300

    logic for parser

--- dice.go
+++ dice.go
@@ -5,6 +5,9 @@ import (
     "os"
     "strings"
     "bufio"
+    "strconv"
+    "math/rand"
+    "github.com/argot42/dice/parser"
 )
 
 func main() {
@@ -17,17 +20,88 @@ func main() {
         r = bufio.NewReader(os.Stdin)
     }
 
-    roll(r)
-}
-
-func roll(in *bufio.Reader) {
     for {
-        r, _, err := in.ReadRune()
+        line, err := r.ReadString('\n')
         if err != nil {
-            fmt.Fprintln(os.Stderr, err)
+            fmt.Fprint(os.Stderr, err)
             os.Exit(1)
         }
 
-        fmt.Print(string(r))
+        n := roll(line)
+
+        fmt.Println(n)
     }
+}
+
+func roll(in string) int {
+    return 0
+}
+
+type DiceListener struct {
+    *parser.BaseDiceListener
+    Stack Stack
+    Random *rand.Rand
+}
+
+type Stack []int
+
+func (s *Stack) Push(n int) {
+    *s = append(*s, n)
+}
+
+func (s *Stack) Pop() (n int) {
+    if len(*s) < 1 {
+        panic("error: Stack is empty")
+    }
+
+    n = (*s)[len(*s)-1]
+    *s = (*s)[:len(*s)-1]
+
+    return
+}
+
+func (l *DiceListener) ExitDic(ctx *parser.DicContext) {
+    dAmount, dType := l.Stack.Pop(), l.Stack.Pop()
+
+    n := 0
+    for i := 0; i < dAmount; i++ {
+        n += l.Random.Intn(dType-1) + 1
+    }
+
+    l.Stack.Push(n)
+}
+
+func (l *DiceListener) ExitMulDiv(ctx *parser.MulDivContext) {
+    right, left := l.Stack.Pop(), l.Stack.Pop()
+
+    switch ctx.GetOp().GetTokenType() {
+    case parser.DiceParserMUL:
+        l.Stack.Push(left * right)
+    case parser.DiceParserDIV:
+        l.Stack.Push(left / right)
+    default:
+        panic(fmt.Sprintf("error: unexpected op: %s", ctx.GetOp().GetText()))
+    }
+}
+
+func (l *DiceListener) ExitAddSub(ctx *parser.AddSubContext) {
+    right, left := l.Stack.Pop(), l.Stack.Pop()
+
+    switch ctx.GetOp().GetTokenType() {
+    case parser.DiceParserADD:
+        l.Stack.Push(left + right)
+    case parser.DiceParserSUB:
+        l.Stack.Push(left - right)
+    default:
+        panic(fmt.Sprintf("error: unexpected op: %s", ctx.GetOp().GetText()))
+    }
+}
+
+func (l *DiceListener) ExitNumber(ctx *parser.NumberContext) {
+    n, err := strconv.Atoi(ctx.GetText())
+    if err != nil {
+        panic(err)
+    }
+
+    l.Stack.Push(n)
 }
